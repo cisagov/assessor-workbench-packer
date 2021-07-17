83264e09cfc5c883bb050a9a41850fc7a6da1b7b
David Redmin <david.redmin@trio.dhs.gov>
2021-07-17 17:15:03 -0400

    Update Terraform to find our assessor portal images

--- terraform-post-packer/main.tf
+++ terraform-post-packer/main.tf
@@ -25,11 +25,11 @@ locals {
 }
 
 # The most-recent AMI created by cisagov/assessor-portal-packer
-data "aws_ami" "example" {
+data "aws_ami" "assessor_portal" {
   filter {
     name = "name"
     values = [
-      "example-hvm-*-x86_64-ebs",
+      "assessor-portal-hvm-*-x86_64-ebs",
     ]
   }
 
@@ -57,5 +57,5 @@ module "ami_launch_permission" {
   }
 
   account_name_regex = local.account_name_regex
-  ami_id             = data.aws_ami.example.id
+  ami_id             = data.aws_ami.assessor_portal.id
 }
